98138ef28b1a51f55a6b80f661fb10edc1996be0
Bishal Kumar Das <bishalkumardasofficial11@gmail.com>
2025-04-07 17:35:44 +0530

    Debug False

--- finance/settings.py
+++ finance/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5zo_&env--5hbok3*@x7dxd3=w5ya_3ubk+7%!m^=!8@k@89e%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["financialnewswebsite-production.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = [
